f16356080b46604af6c8b216a9039c6735a5785f
mevdrr <168742194+mevdrr@users.noreply.github.com>
2026-09-23 01:16:58 +0300

    chore: open pull request comparison after publishing branch

--- PUBLISH_GITHUB.cmd
+++ PUBLISH_GITHUB.cmd
@@ -5,3 +5,4 @@ if errorlevel 1 exit /b 1
 echo.
 echo Branch published. Open this page to create the pull request:
 echo https://github.com/mevdrr/lab_device/compare/main...variant-12-recycle
+start "" "https://github.com/mevdrr/lab_device/compare/main...variant-12-recycle"
